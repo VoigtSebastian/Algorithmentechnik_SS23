9d8b356346bba44da2edb0755c6cd4e0908d4e04
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2023-07-05 11:16:05 +0200

    Make algorithm more greedy

--- greedy/coloring.py
+++ greedy/coloring.py
@@ -5,8 +5,6 @@ a = [[0, 1, 0, 1], [1, 0, 0, 1], [0, 0, 0, 1], [1, 1, 1, 0]]
 n = len(a)
 c = [-1] * n
 
-nc = 0
-
 
 def check_correct():
     for i in range(0, n):
@@ -16,15 +14,12 @@ def check_correct():
     return True
 
 
-while True:
-    c = [-1] * n
-    nc += 1
+for i in range(0, n):
+    cc = 0
+    while True:
+        c[i] = cc
+        if check_correct():
+            break
+        cc += 1
 
-    for i in range(0, n):
-        for cc in range(0, nc):
-            c[i] = cc
-            if check_correct():
-                break
-    if check_correct():
-        print(c)
-        break
+print(c)
