b83fc6a98960acec6bb2ee749108846ad0ec55a8
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2023-07-05 17:07:37 +0200

    Add lists of values

--- divideAndConquer/ggt.py
+++ divideAndConquer/ggt.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+from functools import reduce
+
 
 # Größter gemeinsamer Teiler aka greatest common divisor
 def ggt(a, b):
@@ -12,3 +14,14 @@ def ggt(a, b):
 
 
 assert ggt(132, 28) == 4
+
+# For lists of values, you can either use reduce or a manual method using a for-loop
+# print(reduce(ggt, (132, 28, 4, 8, 24)))
+#
+l = [132, 28, 4, 8, 24]
+t = ggt(l[0], l[1])
+
+for i in l[2:]:
+    t = ggt(i, t)
+
+print(t)
